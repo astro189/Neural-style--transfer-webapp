1272bea269eef45c9c60a8c49c168e9ce7498c3a
astro189 <shirshak.here@gmail.com>
2023-03-19 05:09:21 +0530

    responsive update

--- app.py
+++ app.py
@@ -8,5 +8,10 @@ def home():
     
     return render_template('index.html')
 
+@app.route('/gallery')
+def gallery():
+    
+    return render_template('gallery.html')
+
 if __name__=='__main__':
     app.run(debug=True)
---
+++ static/main.js
@@ -0,0 +1,68 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBDzrMJ7Kr5mgkTcApyxdLSzCPf8mcILCs",
+    authDomain: "neural-style-transfer-86b78.firebaseapp.com",
+    projectId: "neural-style-transfer-86b78",
+    storageBucket: "neural-style-transfer-86b78.appspot.com",
+    messagingSenderId: "291163793715",
+    appId: "1:291163793715:web:d73d85088038bd11e23646",
+    measurementId: "G-0P3JC0VGTC"
+};
+
+firebase.initializeApp(firebaseConfig);
+function uploadImage() {
+    const ref = firebase.storage().ref();
+    const file = document.querySelector("#photo").files[0];
+    const name = +new Date() + "-" + file.name;
+    const metadata = {
+    contentType: file.type
+};
+const task = ref.child(name).put(file, metadata);task
+.then(snapshot => snapshot.ref.getDownloadURL())
+.then(url => {
+alert('image uploaded successfully');
+document.querySelector("#image").src = url;
+})
+.catch(console.error);
+}
+const errorMsgElement = document.querySelector('span#errorMsg');
+
+const img=document.createElement('img')
+
+
+$('#List').find('tbody').html('')
+var i=0
+function getfromeFirebase(){
+    let storageref=firebase.storage().ref()
+
+    storageref.listAll()
+    .then((res)=>{
+        res.items.forEach((imageRef) => {
+            imageRef.getDownloadURL().then(function(url){
+                i++
+                renderImg(i,url)
+            });
+          });
+        })
+    .catch(function (error) {
+        console.log(error);
+    });
+    
+}
+
+
+function renderImg(i,url){
+    console.log(url)
+    var image = new Image();
+    image.src = url;
+    const img=document.getElementById('gallery_container').appendChild(image)
+    img.style.width='6%'
+    img.style.padding='0.5%'
+
+    if(i==12){
+        i=0
+        var next=document.createElement('br')
+        document.getElementById('gallery_container').appendChild(next)
+    }
+
+}
+getfromeFirebase()
