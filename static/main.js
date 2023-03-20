ad92b58964bf809531f5e5c18f0fe84facba2411
astro189 <shirshak.here@gmail.com>
2023-03-21 01:30:58 +0530

    database update

--- static/main.js
+++ static/main.js
@@ -1,3 +1,8 @@
+var percentage
+var uploadPercent=document.querySelector('.percentage')
+var select=document.querySelector('.select')
+const photos=document.querySelector("#photo")
+
 const firebaseConfig = {
     apiKey: "AIzaSyBDzrMJ7Kr5mgkTcApyxdLSzCPf8mcILCs",
     authDomain: "neural-style-transfer-86b78.firebaseapp.com",
@@ -8,16 +13,43 @@ const firebaseConfig = {
     measurementId: "G-0P3JC0VGTC"
 };
 
+var k=0
+function selectImg(){
+    k+=1
+    if(photos.files.length>0){
+        select.innerHTML='File selected'
+    }
+}
+
 firebase.initializeApp(firebaseConfig);
+
+var j=0;
 function uploadImage() {
+    j+=1
+    if(j>=4){
+        uploadPercent.innerHTML="Max upload limit reached"
+        return 0
+    }
+    if(photos.files.length==0){
+        j=0
+        select.innerHTML='No file selected'
+    }
     const ref = firebase.storage().ref();
-    const file = document.querySelector("#photo").files[0];
+    const file = photos.files[0];
     const name = +new Date() + "-" + file.name;
     const metadata = {
     contentType: file.type
 };
-const task = ref.child(name).put(file, metadata);task
-.then(snapshot => snapshot.ref.getDownloadURL())
+const task = ref.child(name).put(file, metadata);
+
+task.on('state_changed',(snapshot)=>{
+    percentage=Math.floor((snapshot.bytesTransferred/snapshot.totalBytes)*100)
+    uploadPercent.innerHTML="Uploading:"+percentage+"%"
+})
+
+task
+.then(snapshot => 
+    snapshot.ref.getDownloadURL())
 .then(url => {
 alert('image uploaded successfully');
 document.querySelector("#image").src = url;
@@ -66,3 +98,5 @@ function renderImg(i,url){
 
 }
 getfromeFirebase()
+
+
